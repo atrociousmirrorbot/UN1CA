e8ae977c5702c4cb7f109791bd9d5f15404d4ff8
atrociousmirrorbot <atrociousmirrorbot@gmail.com>
2025-01-14 16:06:12 +0600

    Update build_odin_package.sh

--- scripts/internal/build_odin_package.sh
+++ scripts/internal/build_odin_package.sh
@@ -123,10 +123,11 @@ echo "Deleting unnecessary images"
 find $TMP_DIR -type f ! -name 'odm.img' ! -name 'product.img' ! -name 'system.img' -delete
 
 echo "Converting all images to xz format"
+sudo apt install -y p7zip-full
 cd $TMP_DIR && for i in *.img; do 7z a -mx9 "${i%.*}.img.xz" "$i" && rm "$i"; done
 
 echo "Installing pypi"
-sudo sudo pip3 install oauth2client google-api-python-client google-auth-httplib2 google-auth-oauthlib
+sudo pip3 install oauth2client google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
 echo "Uploading all .xz files from $TMP_DIR to Google Drive"
 sudo python3 upload.py $TMP_DIR
